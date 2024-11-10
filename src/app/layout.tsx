6239899161ddb03c2b82e1c8707438e5132e3695
dong-wook <boogiewooki02@gmail.com>
2024-11-10 11:15:35 +0900

    [Design] global.css 기존 방식으로 재수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,10 +29,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <Header />
         {/* 375px 이하의 디스플레이인 모든 페이지 양쪽에 20px의 여백 생성 (이후 breakpoint 추가 가능) */}
-        <main className="container">{children}</main>
+        <main className="gutter container">{children}</main>
       </body>
     </html>
   );
