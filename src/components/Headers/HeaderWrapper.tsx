38879e6407b3e7a22fe117e2aaa333087a73215f
dong-wook <boogiewooki02@gmail.com>
2024-11-22 00:41:30 +0900

    Refactor: 헤더 button1 간격 조정, 헤더 불러오는 방식 수정

--- src/components/Headers/HeaderWrapper.tsx
+++ src/components/Headers/HeaderWrapper.tsx
@@ -2,50 +2,59 @@
 
 import Header from "./Header";
 import { usePathname } from "next/navigation";
-import { HeaderProps } from "./Header";
-
-const headerConfig: Record<string, HeaderProps> = {
-  "/": { showButton1: true, title: "hi" },
-  "/page1": { showButton1: true, title: "" },
-  "/page2": { showButton1: true, title: "TEXT텍스트영역" },
-  "/page3": {
-    showButton1: true,
-    title: "TEXT텍스트영역",
-    button2Type: "edit",
-    button2Action: () => {
-      alert("hi");
-    },
-  },
-  "/page4": {
-    showButton1: true,
-    title: "TEXT텍스트영역",
-    button2Type: "complete",
-    button2Action: () => {},
-  },
-  "/page5": {
-    showButton1: false,
-    title: "TEXT텍스트영역",
-    button2Type: "next",
-    button2Action: () => {},
-  },
-  "/page6": {
-    showButton1: true,
-    title: "TEXT텍스트영역",
-    button2Type: "exit",
-    button2Action: () => {},
-  },
-  "/page7": {
-    showButton1: true,
-    title: "TEXT텍스트영역",
-    button2Type: "settings",
-    button2Action: () => {},
-  },
-};
 
 const HeaderWrapper = () => {
   const pathname = usePathname();
+  const getHeaderProps = () => {
+    if (pathname === "/" || pathname === "/page1") {
+      return {
+        showButton1: true,
+        title: "TEXT텍스트영역",
+      };
+    } else if (pathname === "/page2") {
+      return {
+        showButton1: true,
+        title: "TEXT텍스트영역",
+      };
+    } else if (pathname === "/page3") {
+      return {
+        showButton1: true,
+        title: "TEXT텍스트영역",
+        button2Type: "edit",
+        button2Action: () => {},
+      };
+    } else if (pathname === "/page4") {
+      return {
+        showButton1: true,
+        title: "TEXT텍스트영역",
+        button2Type: "complete",
+        button2Action: () => {},
+      };
+    } else if (pathname === "/page5") {
+      return {
+        showButton1: false,
+        title: "TEXT텍스트영역",
+        button2Type: "next",
+        button2Action: () => {},
+      };
+    } else if (pathname === "/page6") {
+      return {
+        showButton1: true,
+        title: "TEXT텍스트영역",
+        button2Type: "exit",
+        button2Action: () => {},
+      };
+    } else if (pathname === "/page7") {
+      return {
+        showButton1: true,
+        title: "TEXT텍스트영역",
+        button2Type: "settings",
+        button2Action: () => {},
+      };
+    }
 
-  const getHeaderProps = () => headerConfig[pathname] || null;
+    return null;
+  };
 
   const headerProps = getHeaderProps();
 
