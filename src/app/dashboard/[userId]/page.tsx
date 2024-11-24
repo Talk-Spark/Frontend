684e2fdacd972e171b54cab5bd505bc3ff0b7086
ocahs9 <piorag99@gmail.com>
2024-11-24 21:19:54 +0900

    chore: 빌드 테스트

--- src/app/dashboard/[userId]/page.tsx
+++ src/app/dashboard/[userId]/page.tsx
@@ -1,8 +1,11 @@
 // src/app/dashboard/[userId]/page.tsx
+
+// 동적 라우팅에서, 페이지의 props(params)는 Promise로 래핑될 가능성이 있으므로
+//Awaited 유틸 사용 필수
 type UserPageProps = {
-  params: {
+  params: Awaited<{
     userId: string;
-  };
+  }>;
 };
 
 export default function UserPage({ params }: UserPageProps) {
