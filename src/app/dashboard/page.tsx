aaaa5e5264d25bf944ac403ca744903815117bcc
kong <piorag99@gmail.com>
2024-12-23 20:32:25 +0900

    feat: api 틀 작성

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,12 @@
 // src/app/dashboard/page.js
+"use client";
+
+import { get, instance } from "@/src/apis";
+import { useEffect } from "react";
+
 export default function DashboardPage() {
+
+
+  
   return <p>This is the dashboard main page.</p>;
 }
