060b60626b6c663b868967fc20d72d1a0f1f2937
kong <piorag99@gmail.com>
2024-12-23 20:38:55 +0900

    chore: 포맷팅

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,56 +2,55 @@ import axios from "axios";
 
 //카카오 로그인 구현하는 사람은 반드시 localStorage에 key 이름을 user로 만들어서 넣어두기 !
 //user : {accessToken: 어쩌구 저쩌구} 형식! / 여기서 accessToken 은 최종적으로 받아와서 앞으로 요청 보낼때 사용할 토큰을 의미
-const getAccessToken = () : string | null => {
-    const user = localStorage.getItem("user");
-    if(user){
-        try{
-            const userObj = JSON.parse(user);
-            return userObj.accessToken || ""
-        }catch(e){
-            console.log("Failed to parse user from localStorage",e)
-            return "";
-        }
+const getAccessToken = (): string | null => {
+  const user = localStorage.getItem("user");
+  if (user) {
+    try {
+      const userObj = JSON.parse(user);
+      return userObj.accessToken || "";
+    } catch (e) {
+      console.log("Failed to parse user from localStorage", e);
+      return "";
     }
-    return "";
-}
+  }
+  return "";
+};
 
 export const instance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-})
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+});
 
 instance.interceptors.request.use((config) => {
-    const token = getAccessToken();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }else{
-        config.headers.Authorization = `Bearer none`; // 응답: malform
-    }
-    return config;
-  });
-
+  const token = getAccessToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    config.headers.Authorization = `Bearer none`; // 응답: malform
+  }
+  return config;
+});
 
 /*
   api 요청 헬퍼 함수 (더욱 간단한 형태로 요청 보낼 수 있도록 작성한 함수)
 */
 //Promise 온전한 타입 추론을 위해 <T>사용
-export function get<T>(...args : Parameters<typeof instance.get>){
-    return instance.get<T>(...args);
+export function get<T>(...args: Parameters<typeof instance.get>) {
+  return instance.get<T>(...args);
 }
 
-export function post<T>(...args : Parameters<typeof instance.post>){
-    return instance.post<T>(...args);
+export function post<T>(...args: Parameters<typeof instance.post>) {
+  return instance.post<T>(...args);
 }
 
-export function put<T>(...args : Parameters<typeof instance.put>){
-    return instance.put<T>(...args);
+export function put<T>(...args: Parameters<typeof instance.put>) {
+  return instance.put<T>(...args);
 }
 
-export function patch<T>(...args : Parameters<typeof instance.patch>){
-    return instance.patch<T>(...args);
+export function patch<T>(...args: Parameters<typeof instance.patch>) {
+  return instance.patch<T>(...args);
 }
 
 //예약어 -> delete가 아니라 del 사용
-export function del<T>(...args : Parameters<typeof instance.delete>){
-    return instance.delete<T>(...args);
+export function del<T>(...args: Parameters<typeof instance.delete>) {
+  return instance.delete<T>(...args);
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,22 +5,20 @@ import { get, instance } from "@/src/apis";
 import { useEffect } from "react";
 
 export default function DashboardPage() {
-
   //사용 예시
-  useEffect(()=>{
-    const getTest = async() => {
-      try{
+  useEffect(() => {
+    const getTest = async () => {
+      try {
         const response = await instance.get("/api/cards/1"); //이건 instance를 사용하는 예시
-        const response2 = await get("/api/cards/1") //이건 헬퍼 함수를 이용하여 더욱 간단하게 api 요청을 하는 방식
+        const response2 = await get("/api/cards/1"); //이건 헬퍼 함수를 이용하여 더욱 간단하게 api 요청을 하는 방식
 
         return response.data;
-      }catch(e){
+      } catch (e) {
         console.log(e);
       }
-    }
+    };
 
     getTest();
-   
-  },[])
+  }, []);
   return <p>This is the dashboard main page.</p>;
 }
