508728dffada4a36809e202cd69852fbc01c9a35
Dongwook <boogiewooki02@gmail.com>
2024-12-28 15:45:26 +0900

    feat: 메인홈 버튼 라우팅

--- src/components/home/MyCard.tsx
+++ src/components/home/MyCard.tsx
@@ -2,6 +2,7 @@
 
 import { ArrowForwardIos } from "@mui/icons-material";
 import StorageNameCard from "../StorageNameCard";
+import { useRouter } from "next/navigation";
 
 const MyCard = () => {
   // 예시 데이터
@@ -14,6 +15,8 @@ const MyCard = () => {
     color: "blue" as "pink" | "green" | "yellow" | "blue",
   };
 
+  const router = useRouter();
+
   const cardId = localStorage.getItem("cardId");
 
   // todo: cardId로 내 명함 정보 가져오기
@@ -22,7 +25,12 @@ const MyCard = () => {
     <div className="my-[3.2rem] flex flex-col gap-[1.6rem]">
       <div className="flex justify-between">
         <div className="text-headline-3 text-black">이름이름 님의 명함</div>
-        <div className="flex items-center text-body-1-med text-gray-7">
+        <div
+          className="flex items-center text-body-1-med text-gray-7"
+          onClick={() => {
+            router.push("/card");
+          }}
+        >
           내 명함
           <ArrowForwardIos
             style={{ fontSize: "2.2rem" }}
