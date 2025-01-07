d3ebf002973e0c58df66c56a1de2e0d92bda395e
Dongwook <boogiewooki02@gmail.com>
2025-01-07 22:46:46 +0900

    feat: 방생성 이후 카카오톡 공유 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import HeaderWrapper from "../components/Headers/HeaderWrapper"; // Client Component
 import type { Metadata } from "next";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,12 @@ export default function RootLayout({
         {/* 375px 이하의 디스플레이인 모든 페이지 양쪽에 20px의 여백 생성 (이후 breakpoint 추가 가능) */}
         <main className="gutter container">{children}</main>
       </body>
+      <Script
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.4/kakao.min.js"
+        integrity="sha384-DKYJZ8NLiK8MN4/C5P2dtSmLQ4KwPaoqAfyA/DfmEc1VDxu4yyC7wy6K1Hs90nka"
+        crossOrigin="anonymous"
+        async
+      />
     </html>
   );
 }
